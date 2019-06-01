7bf331b92af48feeac52e3565fcc757f2af2dcd8
bhped <bhped@126.com>
2019-06-01 16:55:28 +0800

    增加内容

--- git_test/src/cn/itcast/git/GitTest.java
+++ git_test/src/cn/itcast/git/GitTest.java
@@ -4,4 +4,8 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git");
     }
+
+    public void test(){
+        System.out.println("增加内容");
+    }
 }
